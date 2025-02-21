a53872e117cf41f73b77b4478cba0acfdc35d4c6
Diego Moraes <diegompereiradas@gmail.com>
2025-02-21 14:58:22 -0300

    Shuffling and key representation done

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,23 @@
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // Intercambiar elementos
+        [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 }
 
-const estratagemas = [
-    {nombre: "machine-gun", teclas: ["ArrowRight", "ArrowDown", "ArrowUp", "ArrowRight", "ArrowDown"], imagen: "./images/stratagems/machine-gun.svg"},
-    {nombre: "anti-materiel-rifle", teclas: ["ArrowRight", "ArrowUp", "ArrowRight", "ArrowDown"], imagen: "./images/stratagems/anti-materiel-rifle.svg"}
+//Array con todas las estratagemas y sus detalles
+const stratagems = [
+    {name: "machine-gun", keys: ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowRight"], img: "./images/stratagems/machine-gun.svg"},
+    {name: "anti-materiel-rifle", keys: ["ArrowDown", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"], img: "./images/stratagems/anti-materiel-rifle.svg"},
+    {name: "stalwart", keys: ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowUp", "ArrowLeft"], img: "./images/stratagems/stalwart.svg"},
+    {name: "expendable-anti-tank", keys: ["ArrowDown", "ArrowDown", "ArrowLeft", "ArrowUp", "ArrowRight"], img: "./images/stratagems/expendable-anti-tank.svg"},
+    {name: "recoilless-rifle", keys: ["ArrowDown", "ArrowLeft", "ArrowRight", "ArrowRight", "ArrowLeft"], img: "./images/stratagems/recoilless-rifle.svg"},
+    {name: "flamethrower", keys: ["ArrowDown", "ArrowLeft", "ArrowUp", "ArrowDown", "ArrowUp"], img: "./images/stratagems/flamethrower.svg"},
+    {name: "autocannon", keys: ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowUp", "ArrowRight"], img: "./images/stratagems/autocannon.svg"}
 ]
+
+
 
 window.onload = function () {
     
@@ -17,5 +25,22 @@ window.onload = function () {
 
 
 function startGame() {
-    document.getElementById('prueba').innerHTML = `<img src="${estratagemas[Math.floor(Math.random() * estratagemas.length)].imagen}" alt="">`;
+    const shuffledStr = shuffleArray(stratagems);
+    shuffledStr.splice(6);
+
+
+    document.getElementById('cont-str').innerHTML = ""
+    for (let i = 0; i < shuffledStr.length; i++) {
+        if (i == 0){
+            document.getElementById('cont-str').innerHTML += `<img class="first-str stratagem" src="${shuffledStr[i].img}" alt="">`
+        } else {
+            document.getElementById('cont-str').innerHTML += `<img class=" stratagem" src="${shuffledStr[i].img}" alt="">`
+        }
+    }
+
+    document.getElementById('cont-keys').innerHTML = ""
+    for (let i = 0; i < shuffledStr[0].keys.length; i++) {
+        document.getElementById('cont-keys').innerHTML += `<img class="" src="./images/icons/${shuffledStr[0].keys[i]}.png" alt=""></img>`
+    }
+    
 }
