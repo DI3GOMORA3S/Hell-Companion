a00dd577ea6e737742e77ae7739d59b8ab4c55ea
Diego Moraes <diegompereiradas@gmail.com>
2025-03-17 22:03:12 -0300

    More stratagems added

--- public/js/index.js
+++ public/js/index.js
@@ -6,10 +6,9 @@ function shuffleArray(array) {
     return array;
 }
 
-
-
 //Array con todas las estratagemas y sus detalles
 const stratagems = [
+    // Support Weapons
     {name: "machine-gun", keys: ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowRight"], img: "./images/stratagems/machine-gun.svg"},
     {name: "anti-materiel-rifle", keys: ["ArrowDown", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"], img: "./images/stratagems/anti-materiel-rifle.svg"},
     {name: "stalwart", keys: ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowUp", "ArrowLeft"], img: "./images/stratagems/stalwart.svg"},
@@ -22,7 +21,8 @@ const stratagems = [
     {name: "commando", keys: ["ArrowDown", "ArrowLeft", "ArrowUp", "ArrowDown", "ArrowRight"], img: "./images/stratagems/commando.svg"},
     {name: "railgun", keys: ["ArrowDown", "ArrowRight", "ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"], img: "./images/stratagems/railgun.svg"},
     {name: "spear", keys: ["ArrowDown", "ArrowDown", "ArrowUp", "ArrowDown", "ArrowDown"], img: "./images/stratagems/spear.svg"},
-
+ 
+    // Orbital Cannons
     {name: "orbital-gatling-barrage", keys: ["ArrowRight", "ArrowDown", "ArrowLeft", "ArrowUp", "ArrowUp"], img: "./images/stratagems/orbital-gatling-barrage.svg"},
     {name: "orbital-airburst-strike", keys: ["ArrowRight", "ArrowRight", "ArrowRight"], img: "./images/stratagems/orbital-airburst-strike.svg"},
     {name: "orbital-120mm-he-barrage", keys: ["ArrowRight", "ArrowRight", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowDown"], img: "./images/stratagems/orbital-120mm-he-barrage.svg"},
@@ -31,6 +31,16 @@ const stratagems = [
     {name: "orbital-laser", keys: ["ArrowRight", "ArrowDown", "ArrowUp", "ArrowRight", "ArrowDown"], img: "./images/stratagems/orbital-laser.svg"},
     {name: "orbital-napalm-barrage", keys: ["ArrowRight", "ArrowRight", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowUp"], img: "./images/stratagems/orbital-napalm-barrage.svg"},
     {name: "orbital-railcannon-strike", keys: ["ArrowRight", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowRight"], img: "./images/stratagems/orbital-railcannon-strike.svg"},
+
+    // Hangar
+    {name: "eagle-strafing-run", keys: ["ArrowUp", "ArrowRight", "ArrowRight"], img: "./images/stratagems/eagle-strafing-run.svg"},
+    {name: "eagle-airstrike", keys: ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowRight"], img: "./images/stratagems/eagle-airstrike.svg"},
+    {name: "eagle-cluster-bomb", keys: ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowDown", "ArrowRight"], img: "./images/stratagems/eagle-cluster-bomb.svg"},
+    {name: "eagle-napalm-airstrike", keys: ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowUp"], img: "./images/stratagems/eagle-napalm-airstrike.svg"},
+    {name: "jump-pack", keys: ["ArrowDown", "ArrowUp", "ArrowUp", "ArrowDown", "ArrowUp"], img: "./images/stratagems/jump-pack.svg"},
+    {name: "eagle-smoke-strike", keys: ["ArrowUp", "ArrowRight", "ArrowUp", "ArrowDown"], img: "./images/stratagems/eagle-smoke-strike.svg"},
+    {name: "eagle-110mm-rocket-pods", keys: ["ArrowUp", "ArrowRight", "ArrowUp", "ArrowLeft"], img: "./images/stratagems/eagle-110mm-rocket-pods.svg"},
+    {name: "eagle-500kg-bomb", keys: ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowDown", "ArrowDown"], img: "./images/stratagems/eagle-500kg-bomb.svg"},
 ]
 
 let shuffledStr = []
@@ -128,7 +138,6 @@ function showKeySecuence (array){
         document.getElementById('cont-keys').innerHTML += `<img id = "${array[0].name}-${i}" class="notPressed" src="./images/icons/${array[0].keys[i]}.png" alt=""></img>`
     }
 }
-
 
 // #### VARIOUS FUNCTIONS ####
 
